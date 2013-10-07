6c676339d008b5a83d5728c3167436aaff15f016
sonobe@tejimaya <tejimaya@taka-mba.local>
2013-10-07 15:21:27 +0900

    テーマの追加/修正箇所の訂正

---
+++ web/autumn/js/license.js
@@ -0,0 +1,6 @@
+/**
+ * license
+ */
+$(document).ready(function(){
+    $('#license').html('Copyright by <a target"_new" href="http://www.forestwander.com/" rel="nofollow">Forest Wander</a>');
+  });
---
+++ web/winter/js/license.js
@@ -0,0 +1,6 @@
+/**
+ * license
+ */
+$(document).ready(function(){
+    $('#license').html('Copyright by <a target"_new" href="http://earthincolors.wordpress.com" rel="nofollow">Moyan Brenn</a>');
+  });
