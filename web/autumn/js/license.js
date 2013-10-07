6c7a83413194e65d174868429255239c4ee26fe0
kaoru nishizoe <nishizoe@tejimaya.com>
2013-10-07 16:37:20 +0900

    removed unnecessary spaces

--- web/autumn/js/license.js
+++ web/autumn/js/license.js
@@ -3,4 +3,4 @@
  */
 $(document).ready(function(){
     $('#license').html('Copyright by <a target"_new" href="http://www.forestwander.com/" rel="nofollow">Forest Wander</a>');
-  });
+});
--- web/winter/js/license.js
+++ web/winter/js/license.js
@@ -3,4 +3,4 @@
  */
 $(document).ready(function(){
     $('#license').html('Copyright by <a target"_new" href="http://earthincolors.wordpress.com" rel="nofollow">Moyan Brenn</a>');
-  });
+});
