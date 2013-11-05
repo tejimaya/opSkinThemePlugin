78b64f04fac1aa79d377e4c48b5d11c2b2385c7e
kaoru nishizoe <nishizoe@tejimaya.com>
2013-11-05 11:52:18 +0900

    テストを追加

--- test/functional/pc_frontend/opSkinThemePluginActionsTest.php
+++ test/functional/pc_frontend/opSkinThemePluginActionsTest.php
@@ -3,13 +3,14 @@
 include dirname(__FILE__).'/../../bootstrap/functional.php';
 
 $browser = new opTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
-$browser->setMobile();
 
 include dirname(__FILE__).'/../../bootstrap/database.php';
 
 $browser->login('sns@example.com', 'password');
 $browser->setCulture('en');
 
-$browser->get('/')
-  ->with('user')->isAuthenticated()
-;
+$browser->get('/skinpreview/index/theme_name/autumn');
+$browser->get('/skinpreview/index/theme_name/');
+$browser->get('/skinpreview/index/theme_name');
+$browser->get('/skinpreview/index');
+$browser->get('/skinpreview');
