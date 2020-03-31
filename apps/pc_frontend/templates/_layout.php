69b85f91ab31af92eec3f2953e910c0bd1ba3af0
nishizoe <nishizoe@tejimaya.com>
2020-03-31 16:35:40 +0900

    (fixed #4424) 自分以外のメンバーのフレンドリストのメニュー部の背景色が自分向けの背景色になっているのを修正

--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -37,7 +37,11 @@ var openpne = '.json_encode($jsonData).';
 <div id="Body">
 <?php echo $op_config->get('pc_html_top') ?>
 <div id="Container">
-<?php $type = sfConfig::get('sf_nav_type', sfConfig::get('mod_'.$module.'_default_nav', 'default')); ?>
+<?php
+$context = sfContext::getInstance();
+$module = $context->getActionStack()->getLastEntry()->getModuleName();
+$type = sfConfig::get('sf_nav_type', sfConfig::get('mod_'.$module.'_default_nav', 'default'));
+?>
 <div id="Header" class="navbar <?php if('friend' == $type): ?>navbar-inverse<?php endif; ?>">
 <div id="HeaderContainer" div class="navbar-inner">
 <div class="container">
